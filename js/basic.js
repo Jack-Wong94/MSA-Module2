1e9d7c6130394243e5ae38dfdfc3bf682f163833
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2016-08-29 22:58:06 +1200

    Create game button constructor

--- js/basic.js
+++ js/basic.js
@@ -2,3 +2,9 @@ function start() {
     $("#contentContainer").hide();
     $("#gameContainer").show();
 }
+var GameButton = (function () {
+    function GameButton(id) {
+        this.id = id;
+    }
+    return GameButton;
+})();
--- js/basic.ts
+++ js/basic.ts
@@ -2,3 +2,10 @@ function start(){
     $("#contentContainer").hide();
     $("#gameContainer").show();
 }
+class GameButton{
+    id:number;
+    constructor(id: number){
+        this.id = id;
+    }
+    
+}
