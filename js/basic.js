0b1466eae0b59e390e4ff0cc27506179141f4f6f
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2016-08-30 11:59:29 +1200

    Chagne the button display when clicked (not finished)

--- js/basic.js
+++ js/basic.js
@@ -1,10 +1,28 @@
 function start() {
     $("#contentContainer").hide();
     $("#gameContainer").show();
+
+    var gameBtn = new GameButton("1");
+    gameBtn.setGameButton(6);
 }
+function getPicture(id) {
+    document.getElementById(id).innerHTML = "5";
+}
+$(document).ready(function () {
+    $(".BtnGame").click(function () {
+        alert("The paragraph was clicked.");
+    });
+});
+
 var GameButton = (function () {
     function GameButton(id) {
         this.id = id;
     }
+    GameButton.prototype.getGameButton = function (button) {
+        return button;
+    };
+    GameButton.prototype.setGameButton = function (data) {
+        this.data = data;
+    };
     return GameButton;
 })();
--- js/basic.ts
+++ js/basic.ts
@@ -1,11 +1,30 @@
 function start(){
     $("#contentContainer").hide();
     $("#gameContainer").show();
+
+    var gameBtn = new GameButton("1");
+    gameBtn.setGameButton(6);
 }
+function getPicture(id:string){
+    
+    document.getElementById(id).innerHTML="5";
+}
+$(document).ready(function(){
+    $(".BtnGame").click(function(){
+        alert("The paragraph was clicked.");
+    });
+});
+
 class GameButton{
-    id:number;
-    constructor(id: number){
+    id:string;
+    data:number;
+    constructor(id: string){
         this.id = id;
     }
-    
+    getGameButton(button:GameButton){
+        return button;
+    }
+    setGameButton(data:number){
+        this.data = data;
+    }
 }
