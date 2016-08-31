65e598798c6e5b038c13d44cd878cfd65be3db0a
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2016-09-01 00:07:02 +1200

    add return to menu button

--- js/main.ts
+++ js/main.ts
@@ -1,7 +1,35 @@
 var BtnGame = $(".BtnGame");
+var BtnStart = $("#BtnStart");
+var BtnMenu = $("#BtnMenu");
+var contentContainer = $("#contentContainer");
+var gameContainer = $("#gameContainer");
+
+BtnMenu.on("click",function(){
+    swal({
+        title:"Are you sure you want to go back to menu?",
+        type:"info",
+        showCancelButton:true,
+        closeOnConfirm:false,
+        showLoaderOnConfirm:true,
+    },
+    function(){
+        gameContainer.hide();
+        contentContainer.show();
+    });
+   
+});
+
 BtnGame.on("click",function(){
     sweetAlert("sdfsdf");
 });
+
+BtnStart.on("click",function(){
+    contentContainer.hide();
+    gameContainer.show();
+    BtnMenu.show();
+
+});
+
 function start(){
     $("#contentContainer").hide();
     $("#gameContainer").show();
