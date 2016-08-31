f856e1a273b99d3aae9b753b154b22051c3199ba
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2016-09-01 00:35:03 +1200

    Update game button to return unique id

--- js/main.js
+++ js/main.js
@@ -9,16 +9,16 @@ BtnMenu.on("click", function () {
         title: "Are you sure you want to go back to menu?",
         type: "info",
         showCancelButton: true,
-        closeOnConfirm: false,
+        closeOnConfirm: true,
         showLoaderOnConfirm: true
     }, function () {
         gameContainer.hide();
         contentContainer.show();
+        BtnMenu.hide();
     });
 });
 
 BtnGame.on("click", function () {
-    sweetAlert("sdfsdf");
 });
 
 BtnStart.on("click", function () {
@@ -30,12 +30,14 @@ BtnStart.on("click", function () {
 function start() {
     $("#contentContainer").hide();
     $("#gameContainer").show();
-
+    for (var i = 1; i <= 18; i++) {
+        var gameBtn = new GameButton(i.toString());
+    }
     var gameBtn = new GameButton("1");
     gameBtn.setGameButton(6);
 }
 function getPicture(id) {
-    document.getElementById(id).innerHTML = "5";
+    document.getElementById(id).innerHTML = id;
 }
 
 var GameButton = (function () {
--- js/main.ts
+++ js/main.ts
@@ -9,18 +9,20 @@ BtnMenu.on("click",function(){
         title:"Are you sure you want to go back to menu?",
         type:"info",
         showCancelButton:true,
-        closeOnConfirm:false,
+        closeOnConfirm:true,
         showLoaderOnConfirm:true,
     },
     function(){
         gameContainer.hide();
         contentContainer.show();
+        BtnMenu.hide();
+        
     });
    
 });
 
 BtnGame.on("click",function(){
-    sweetAlert("sdfsdf");
+   
 });
 
 BtnStart.on("click",function(){
@@ -33,13 +35,15 @@ BtnStart.on("click",function(){
 function start(){
     $("#contentContainer").hide();
     $("#gameContainer").show();
-
+    for (var i = 1; i<=18; i++){
+        var gameBtn = new GameButton(i.toString());
+    }
     var gameBtn = new GameButton("1");
     gameBtn.setGameButton(6);
 }
 function getPicture(id:string){
     
-    document.getElementById(id).innerHTML="5";
+    document.getElementById(id).innerHTML=id;
 }
 
 
