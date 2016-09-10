a62b00363175f1af2f8a12bb2cdcea2b6af07ffd
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2016-09-10 22:57:03 +1200

    Initalise facebook login

--- js/main.ts
+++ js/main.ts
@@ -82,3 +82,27 @@ class GameButton{
         this.data = data;
     }
 }
+
+ window.fbAsyncInit = function() {
+                FB.init({
+                appId      : '1752409965009814',
+                 xfbml      : true,
+                version    : 'v2.7'
+                });
+                FB.getLogInStatus(function(response){
+                    if (response.status === 'connected'){
+                        document.getElementById('status').innerHTML = 'We are connected.';
+                    }else if (response.status === 'not_authorized'){
+                        document.getElementById('status').innerHTML = 'We are not logged in.'
+                    }
+                });
+            };
+
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+
